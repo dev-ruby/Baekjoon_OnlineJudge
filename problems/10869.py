a19f2a23c65c9e8940ccfb5b0d0393433824cacf
DevRuby <hiveruby@gmail.com>
2022-10-11 21:16:46 +0900

    10869 - 사칙연산

---
+++ problems/10869.py
@@ -0,0 +1,14 @@
+# 10869 - 사칙연산
+# 2022-10-11
+
+
+def solve():
+    A, B = map(int, input().split())
+    print(A + B)
+    print(A - B)
+    print(A * B)
+    print(A // B)
+    print(A % B)
+
+
+solve()
